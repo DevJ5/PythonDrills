deed4d0ef0a361070209a5af6699dfc1db5045d6
DevJ5 <idevelopjs@gmail.com>
2020-01-25 17:47:07 +0100

    CheckPrimalFunctionality

---
+++ CheckPrimalityFunctions.py
@@ -0,0 +1,28 @@
+"""
+Ask the user for a number and determine whether the number is prime or not. 
+(For those who have forgotten, a prime number is a number that has no divisors.). 
+You can (and should!) use your answer to Exercise 4 to help you. 
+Take this opportunity to practice using functions, described below.
+"""
+
+def getNumber(helptext = "Enter a number\n$ "):
+    return int(input(helptext))
+
+def checkIsPrime(number):
+    divisor = 2
+    isPrime = True
+    
+    while divisor != number:
+       if number % divisor == 0:
+           isPrime = False
+           break
+       divisor += 1
+    return isPrime
+
+def main():
+    number = getNumber()
+    print(checkIsPrime(number))
+
+if __name__ == "__main__":
+    main()
+
