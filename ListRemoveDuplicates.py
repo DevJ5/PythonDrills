b515d542ac3e55eb2fa092c571038527e4116289
DevJ5 <idevelopjs@gmail.com>
2020-02-04 21:00:59 +0100

    ListRemoveDuplicates

--- ListRemoveDuplicates.py
+++ ListRemoveDuplicates.py
@@ -9,7 +9,27 @@ Go back and do Exercise 5 using sets, and write the solution for that in a diffe
 """
 
 def main():
-    
+    list = []
+    lookup = {}
+
+    while True:
+        input = getInput("Enter a number or type 'q' to return a list of non duplicates\n")
+        if input == 'q':
+            break
+        if checkNoDuplicate(lookup, input):
+            addToList(list, input)
+            lookup[input] = 1
+
+    print(list)
+
+def getInput(message):
+    return input(message)
+
+def checkNoDuplicate(lookup, input):
+    return lookup.get(input) == None
+
+def addToList(list, item):
+    list.append(int(item))
 
 if __name__ == "__main__":
     main()
